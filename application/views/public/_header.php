135bac8b81d125b16d4f0278a6696c416decd378
mks6804 <host@fearlessflyer.com>
2013-03-17 12:47:11 -0700

    testing git commit

--- application/views/public/_header.php
+++ application/views/public/_header.php
@@ -52,7 +52,7 @@
 <div class="nav">
 <ul>
     <li><a href="<?php echo base_url();?>">Home</a></li>
-    <li><a href="<?php echo base_url();?>about/">About</a></li>
+    <li><a href="<?php echo base_url();?>about/">About Us</a></li>
     <li><a href="<?php echo base_url();?>menu/">Our Menu</a></li> 
     <li><a href="<?php echo base_url();?>contact/">Contact</a></li> 
     <li><a href="<?php echo base_url();?>order/">
